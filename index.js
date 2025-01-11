1ded8bae531654944f485a31adbce8bae7ed7fea
Chirag-04 <04chirag2001@gmail.com>
2025-01-11 18:43:18 +0530

    changed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 connectDB();
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.json({
     message: "Welcome to the Crypto API!",
     guide: [
