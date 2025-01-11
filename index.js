054ee59a89295ac48336a2f58d50b574186243ac
Chirag-04 <04chirag2001@gmail.com>
2025-01-11 18:21:04 +0530

    updated index.js

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 connectDB();
 
+app.use("/", (req, res) => {
+  res.json({
+    message: "Welcome to the Crypto API!",
+    guide: {
+      "GET /": "Lists all available API endpoints and their descriptions.",
+      "GET /stats": "Fetches statistical data about cryptocurrencies, including aggregated metrics.",
+      "GET /deviation": "Calculates and provides the price deviation of cryptocurrencies over a specific period.",
+    },
+  });
+});
+
+
 app.use('/api',cryptoRouter );
 
 app.listen(port, () => {
